ade75d278cdf9cac1fe3d65b135d3dac6bd7bcdf
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-02-08 10:22:56 +0100

    Commit 2

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log("It works!");
+// string
+var myName = "Max";
+// myName = 28;
+// numbers
+var myAge = 27.5;
+// myAge: "Max";
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = "27";
+// array
+var hobbies = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100;
+// tuples
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: "3" };
+console.log(car);

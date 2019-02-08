01bf70ed3ea0c7c39da860011c8ea7970c7ff9be
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-02-08 17:03:11 +0100

    Module 3

--- app.js
+++ app.js
@@ -1,98 +1,42 @@
-"use strict";
-// string
-var myName = "Max";
-// myName = 28;
-// numbers
-var myAge = 27.5;
-// myAge: "Max";
-// boolean
-var hasHobbies = false;
-// hasHobbies = 1;
-// assign types
-var myRealAge;
-myRealAge = 27;
-// myRealAge = "27";
-// array
-var hobbies = ["Cooking", "Sports"];
-hobbies = [100];
-// hobbies = 100;
-// tuples
-var address = ["Superstreet", 99];
-// enum
-var Color;
-(function (Color) {
-    Color[Color["Gray"] = 0] = "Gray";
-    Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 101] = "Blue"; // 2
-})(Color || (Color = {}));
-var myColor = Color.Blue;
-console.log(myColor);
-// any
-var car = "BMW";
-console.log(car);
-car = { brand: "BMW", series: "3" };
-console.log(car);
-// functions
-function returnMyName() {
-    return myName;
-}
-console.log(returnMyName());
-// void
-function sayHello() {
-    console.log("Hello");
-}
-// argument types
-function multiply(value1, value2) {
-    return value1 * value2;
-}
-// console.log(multiply(2, "max"));
-console.log(multiply(10, 2));
-// function types
-var myMultiply;
-// myMultiply = sayHello;
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(5, 2));
-// objects
-var userData = {
-    name: "Max",
-    age: 27
-};
-// userData = {
-//   a: "Hello",
-//   b: 22
+// // Arrow Functions
+// console.log("Arrow Functions");
+// const addNumbers = function(number1: number, number2: number): number {
+//   return number1 + number2;
 // };
-// complex object
-var complex = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
-};
-// complex object
-var complex2 = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
-};
-// Union Types
-var myRealRealAge = 28;
-myRealRealAge = "27";
-// myRealRealAge = true;
-// check types
-var finalValue = 30;
-if (typeof finalValue == "number") {
-    console.log("Final value is a number");
-}
-// never
-function neverReturns() {
-    throw new Error("An error!");
-}
-// Nullable Types
-var canBeNull = 12;
-canBeNull: null;
-var canAlsoBeNull;
-canAlsoBeNull: null;
-var canThisBeAny = null;
-canThisBeAny = 12;
+// console.log(addNumbers(10, 3));
+// const multiplyNumbers = (number1: number, number2: number): number =>
+//   number1 * number2;
+// console.log(multiplyNumbers(10, 3));
+// const greet = () => {
+//   console.log("Hello");
+// };
+// greet();
+// const greetFriend = (friend: string) => console.log(friend);
+// greetFriend("Manu");
+// // Default Parameters
+// console.log("DEFAULT PARAMETERS");
+// const countdown = (start: number = 10): void => {
+//   console.log(start);
+//   while (start > 0) {
+//     start--;
+//   }
+//   console.log("Done!", start);
+// };
+// countdown();
+// // REST & SPREAD
+// console.log("REST & SPREAD");
+// const numbers = [1, 10, 99, -5];
+// console.log(Math.max(33, 99, 10, -3));
+// console.log(Math.max(...numbers));
+// function makeArray(...args: number[]) {
+//   return args;
+// }
+// console.log(makeArray(1, 2, 6));
+// // Destructuring
+// console.log("DESTRUCTURING");
+// const myHobbies = ["Cooking", "Sports"];
+// const [hobby1, hobby2] = myHobbies;
+// console.log(hobby1, hobby2);
+// const userData = { userName: "Lillo", age: 33 };
+// const { userName: myName, age: myAge } = userData;
+// console.log(myName, myAge);
--- app.ts
+++ app.ts
@@ -1,124 +1,56 @@
-// string
-let myName: string = "Max";
-// myName = 28;
+// // Arrow Functions
+// console.log("Arrow Functions");
 
-// numbers
-let myAge: number = 27.5;
-// myAge: "Max";
-
-// boolean
-let hasHobbies: boolean = false;
-// hasHobbies = 1;
-
-// assign types
-let myRealAge: number;
-myRealAge = 27;
-// myRealAge = "27";
-
-// array
-let hobbies: any[] = ["Cooking", "Sports"];
-hobbies = [100];
-// hobbies = 100;
-
-// tuples
-let address: [string, number] = ["Superstreet", 99];
-
-// enum
-enum Color {
-  Gray, // 0
-  Green = 100, // 1
-  Blue // 2
-}
-
-let myColor: Color = Color.Blue;
-console.log(myColor);
-
-// any
-let car: any = "BMW";
-console.log(car);
-car = { brand: "BMW", series: "3" };
-console.log(car);
-
-// functions
-function returnMyName(): string {
-  return myName;
-}
-
-console.log(returnMyName());
-
-// void
-function sayHello(): void {
-  console.log("Hello");
-}
-
-// argument types
-function multiply(value1: number, value2: number): number {
-  return value1 * value2;
-}
-
-// console.log(multiply(2, "max"));
-console.log(multiply(10, 2));
-
-// function types
-let myMultiply: (val1: number, val2: number) => number;
-// myMultiply = sayHello;
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(5, 2));
-
-// objects
-let userData: { name: string; age: number } = {
-  name: "Max",
-  age: 27
-};
-
-// userData = {
-//   a: "Hello",
-//   b: 22
+// const addNumbers = function(number1: number, number2: number): number {
+//   return number1 + number2;
 // };
 
-// complex object
-let complex: { data: number[]; output: (all: boolean) => number[] } = {
-  data: [100, 3.99, 10],
-  output: function(all: boolean): number[] {
-    return this.data;
-  }
-};
+// console.log(addNumbers(10, 3));
 
-// complex = {};
+// const multiplyNumbers = (number1: number, number2: number): number =>
+//   number1 * number2;
 
-// type alias
-type Complex = { data: number[]; output: (all: boolean) => number[] };
+// console.log(multiplyNumbers(10, 3));
 
-// complex object
-let complex2: Complex = {
-  data: [100, 3.99, 10],
-  output: function(all: boolean): number[] {
-    return this.data;
-  }
-};
+// const greet = () => {
+//   console.log("Hello");
+// };
 
-// Union Types
-let myRealRealAge: number | string = 28;
-myRealRealAge = "27";
-// myRealRealAge = true;
+// greet();
 
-// check types
-let finalValue = 30;
-if (typeof finalValue == "number") {
-  console.log("Final value is a number");
-}
+// const greetFriend = (friend: string) => console.log(friend);
+// greetFriend("Manu");
 
-// never
-function neverReturns(): never {
-  throw new Error("An error!");
-}
+// // Default Parameters
+// console.log("DEFAULT PARAMETERS");
+// const countdown = (start: number = 10): void => {
+//   console.log(start);
+//   while (start > 0) {
+//     start--;
+//   }
+//   console.log("Done!", start);
+// };
 
-// Nullable Types
-let canBeNull: number | null = 12;
-canBeNull: null;
-let canAlsoBeNull;
-canAlsoBeNull: null;
+// countdown();
 
-let canThisBeAny: number | null = null;
-canThisBeAny = 12;
+// // REST & SPREAD
+// console.log("REST & SPREAD");
+// const numbers = [1, 10, 99, -5];
+// console.log(Math.max(33, 99, 10, -3));
+// console.log(Math.max(...numbers));
+
+// function makeArray(...args: number[]) {
+//   return args;
+// }
+
+// console.log(makeArray(1, 2, 6));
+
+// // Destructuring
+// console.log("DESTRUCTURING");
+// const myHobbies = ["Cooking", "Sports"];
+// const [hobby1, hobby2] = myHobbies;
+// console.log(hobby1, hobby2);
+
+// const userData = { userName: "Lillo", age: 33 };
+// const { userName: myName, age: myAge } = userData;
+// console.log(myName, myAge);

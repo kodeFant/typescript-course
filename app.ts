9459f0047b50c90dba68f1cf81f087bbceddde2b
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2019-02-08 13:22:54 +0100

    Finished module 2

--- app.ts
+++ app.ts
@@ -16,25 +16,109 @@ myRealAge = 27;
 // myRealAge = "27";
 
 // array
-let hobbies: any[] = ["Cooking", "Sports"]
-hobbies = [100]
+let hobbies: any[] = ["Cooking", "Sports"];
+hobbies = [100];
 // hobbies = 100;
 
 // tuples
-let address: [string, number] = ["Superstreet", 99]
+let address: [string, number] = ["Superstreet", 99];
 
 // enum
 enum Color {
-    Gray,   // 0
-    Green = 100,  // 1
-    Blue    // 2
+  Gray, // 0
+  Green = 100, // 1
+  Blue // 2
 }
 
 let myColor: Color = Color.Blue;
-console.log(myColor)
+console.log(myColor);
 
 // any
 let car: any = "BMW";
-console.log(car)
-car = { brand: "BMW", series: "3" }
-console.log(car)
+console.log(car);
+car = { brand: "BMW", series: "3" };
+console.log(car);
+
+// functions
+function returnMyName(): string {
+  return myName;
+}
+
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+  console.log("Hello");
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+
+// console.log(multiply(2, "max"));
+console.log(multiply(10, 2));
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+
+// objects
+let userData: { name: string; age: number } = {
+  name: "Max",
+  age: 27
+};
+
+// userData = {
+//   a: "Hello",
+//   b: 22
+// };
+
+// complex object
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+  data: [100, 3.99, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// complex = {};
+
+// type alias
+type Complex = { data: number[]; output: (all: boolean) => number[] };
+
+// complex object
+let complex2: Complex = {
+  data: [100, 3.99, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// Union Types
+let myRealRealAge: number | string = 28;
+myRealRealAge = "27";
+// myRealRealAge = true;
+
+// check types
+let finalValue = 30;
+if (typeof finalValue == "number") {
+  console.log("Final value is a number");
+}
+
+// never
+function neverReturns(): never {
+  throw new Error("An error!");
+}
+
+// Nullable Types
+let canBeNull: number | null = 12;
+canBeNull: null;
+let canAlsoBeNull;
+canAlsoBeNull: null;
+
+let canThisBeAny: number | null = null;
+canThisBeAny = 12;
